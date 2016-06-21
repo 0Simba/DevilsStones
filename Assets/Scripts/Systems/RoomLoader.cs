afe61bcf60e5083a355f0d51eb271b885e7d136b
0Simba <b.dersoir@student.isartdigital.com>
2016-06-21 22:37:05 +0200

    Pulling system for map loader

--- Assets/Scripts/Systems/RoomLoader.cs
+++ Assets/Scripts/Systems/RoomLoader.cs
@@ -13,7 +13,7 @@ public class RoomLoader : MonoBehaviour {
     static public void Load (RoomData data) {
         currentData = data;
 
-        instance.AsyncLoad();
+        instance.Map();
     }
 
 
@@ -36,6 +36,7 @@ public class RoomLoader : MonoBehaviour {
     public int[][]          cursors;
 
 
+
     void Awake () {
         instance = this;
     }
@@ -47,9 +48,45 @@ public class RoomLoader : MonoBehaviour {
     }
 
 
+    void Map () {
+        DeactivateAll();
+        currentData.ForEachTile(PlaceTile);
+    }
+
+
+    void PlaceTile (int x, int y, Tile tile) {
+        int        index    = cursors[(int) tile.type][tile.appearence];
+        GameObject target   = pulling[(int) tile.type][tile.appearence][index];
+
+        cursors[(int) tile.type][tile.appearence]++;
+
+        target.SetActive(true);
+        target.transform.position = WorldPosition(x, y);
+    }
+
+
+
+
+    void DeactivateAll() {
+        for (int typeIndex = 0; typeIndex < cursors.Length; ++typeIndex) {
+            for (int appearenceIndex = 0; appearenceIndex < cursors[typeIndex].Length; ++appearenceIndex) {
+                while (cursors[typeIndex][appearenceIndex] > 0) {
+
+                    int        lastIndex = cursors[typeIndex][appearenceIndex] - 1;
+                    GameObject current   = pulling[typeIndex][appearenceIndex][lastIndex];
+
+                    current.SetActive(false);
+                    cursors[typeIndex][appearenceIndex]--;
+                }
+            }
+        }
+    }
+
+
     void CreatePullingArray () {
         int tileTypes = Enum.GetNames(typeof(Tile.Type)).Length;
         pulling = new GameObject[tileTypes][][];
+        cursors = new int[tileTypes][];
 
         for (int i = 0; i < tileTypes; ++i) {
             CreatePullingForTileTypes(i);
@@ -61,54 +98,24 @@ public class RoomLoader : MonoBehaviour {
         int appearences = config.typeToPrefabList[typeIndex].Length;
 
         pulling[typeIndex] = new GameObject[appearences][];
+        cursors[typeIndex] = new int[appearences];
 
         int instanceToCreate = config.typeToNumberMax[typeIndex];
 
         for (int i = 0; i < appearences; i++) {
-            FillOfPrefab(pulling[typeIndex][i], instanceToCreate, config.typeToPrefabList[typeIndex][i]);
+            cursors[typeIndex][i] = 0;
+            FillOfPrefab(typeIndex, i, instanceToCreate);
         }
     }
 
 
-    void FillOfPrefab (GameObject[] array, int number, GameObject prefab) {
-        array = new GameObject[number];
+    void FillOfPrefab (int typeIndex, int appearenceIndex, int number) {
+        pulling[typeIndex][appearenceIndex] = new GameObject[number];
+        GameObject prefab = config.typeToPrefabList[typeIndex][appearenceIndex];
 
         for (int i = 0; i < number; ++i) {
             GameObject instanciatedPrefab = GameObject.Instantiate(prefab) as GameObject;
-            array[i] = instanciatedPrefab;
+            pulling[typeIndex][appearenceIndex][i] = instanciatedPrefab;
         }
     }
-
-
-    public void AsyncLoad () {
-        VoidChilds();
-        StartCoroutine(LoadCoroutine());
-    }
-
-
-    public void VoidChilds () {
-        foreach (Transform child in transform) {
-            Destroy(child.gameObject);
-        }
-    }
-
-
-    private IEnumerator LoadCoroutine () {
-        for (int x = 0; x < currentData.sizeX; ++x) {
-            for (int y = 0; y < currentData.sizeY; ++y) {
-                Spawn(x, y, currentData.tiles[x, y]);
-                yield return null;
-            }
-        }
-    }
-
-
-    public void Spawn (int x, int y, Tile tile) {
-        Vector3 position = WorldPosition(x, y);
-        GameObject instiatedTile = GameObject.Instantiate(GetGameObjectPrefab(tile), position, Quaternion.identity) as GameObject;
-
-        instiatedTile.transform.SetParent(transform);
-    }
-
-
 }
